22097012fd968aa7e397d77cdbed8bf0cbdde621
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-06-23 13:18:49 +0200

    Adding some extra fields for sending kafka properties key, partition and timestamp

--- src/main/java/com/biit/kafka/events/KafkaEventTemplate.java
+++ src/main/java/com/biit/kafka/events/KafkaEventTemplate.java
@@ -21,4 +21,28 @@ public class KafkaEventTemplate<K, V extends Event<?>> extends KafkaTemplate<K, 
     public CompletableFuture<SendResult<K, V>> send(@Nullable V data) {
         return super.send(kafkaConfig.getKafkaTopic(), data);
     }
+
+    @Override
+    public CompletableFuture<SendResult<K, V>> send(@Nullable String topic, @Nullable V data) {
+        if (topic == null) {
+            return super.send(kafkaConfig.getKafkaTopic(), data);
+        } else {
+            return super.send(topic, data);
+        }
+    }
+
+    public CompletableFuture<SendResult<K, V>> send(@Nullable String topic, @Nullable K key, @Nullable Integer partition,
+                                                    @Nullable Long timestamp, @Nullable V data) {
+        if (topic == null || topic.isBlank()) {
+            topic = kafkaConfig.getKafkaTopic();
+        }
+        if (key == null) {
+            return super.send(topic, data);
+        } else if (partition == null) {
+            return super.send(topic, key, data);
+        } else if (timestamp == null) {
+            return super.send(topic, partition, key, data);
+        }
+        return super.send(topic, partition, timestamp, key, data);
+    }
 }
