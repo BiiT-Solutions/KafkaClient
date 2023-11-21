42c095a191b2ab5ec0f922d6bc19e0319d162a6d
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-11-21 16:21:29 +0100

    Added EventDTO

---
+++ kafka-client-dto/src/main/java/com/biit/kafka/controller/models/EventDTO.java
@@ -0,0 +1,164 @@
+package com.biit.kafka.controller.models;
+
+import com.biit.server.controllers.models.CreatedElementDTO;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+public class EventDTO extends CreatedElementDTO {
+
+    private String id;
+
+    private String to;
+
+    private String replyTo;
+
+    private UUID sessionId;
+
+    private UUID messageId;
+
+    private UUID correlationId;
+
+    private String subject;
+
+    private String tag;
+
+    private String tenant;
+
+    private String contentType;
+
+    private String authorization;
+
+    private String entityType;
+
+    private Object payload;
+
+    private Map<String, String> customProperties;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getReplyTo() {
+        return replyTo;
+    }
+
+    public void setReplyTo(String replyTo) {
+        this.replyTo = replyTo;
+    }
+
+    public UUID getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(UUID sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public UUID getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(UUID messageId) {
+        this.messageId = messageId;
+    }
+
+    public UUID getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(UUID correlationId) {
+        this.correlationId = correlationId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(String tenant) {
+        this.tenant = tenant;
+    }
+
+    public String getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
+
+    public String getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    public LocalDateTime getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    public void setCreatedAt(LocalDateTime createAt) {
+        super.setCreatedAt(createAt);
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+    public Map<String, String> getCustomProperties() {
+        return customProperties;
+    }
+
+    public void setCustomProperties(Map<String, String> customProperties) {
+        this.customProperties = customProperties;
+    }
+}
