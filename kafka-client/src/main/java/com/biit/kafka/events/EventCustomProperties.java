eb07fbd2ac9a9706fb0cdc2c0e09d69c6e95df83
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-07-30 16:01:35 +0200

    Added parameter Tag

--- kafka-client/src/main/java/com/biit/kafka/events/EventCustomProperties.java
+++ kafka-client/src/main/java/com/biit/kafka/events/EventCustomProperties.java
@@ -9,6 +9,9 @@ public enum EventCustomProperties {
     //Label from the source.
     SOURCE_TAG("sourceTag"),
 
+    //If it is a variable or parameter from source.
+    PARAMETER_TAG("parameterTag"),
+
     //Deprecated, use organization event property.
     @Deprecated
     ORGANIZATION("organization");
