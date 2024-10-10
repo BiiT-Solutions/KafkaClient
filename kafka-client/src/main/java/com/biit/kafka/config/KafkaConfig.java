c816cf61c0d314e2e7332770e5198f6ed47575d0
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-10-10 16:19:38 +0200

    Increased packet buffer size

--- kafka-client/src/main/java/com/biit/kafka/config/KafkaConfig.java
+++ kafka-client/src/main/java/com/biit/kafka/config/KafkaConfig.java
@@ -143,6 +143,7 @@ public class KafkaConfig {
             props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, kafkaMessageMaxBytes);
             props.put("buffer.memory", kafkaMessageMaxBytes);
             props.put("message.max.bytes", kafkaMessageMaxBytes);
+            props.put("max.message.bytes", kafkaMessageMaxBytes);
         }
         if (kafkaCompressionType != null && !kafkaCompressionType.isEmpty()) {
             //If sending json payloads, snappy compression is recommended. But on alpine is needed 'apk add --no-cache gcompat'
